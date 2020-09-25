708a5eb409a22ebcbe1a0ff727f3dcc8a9c427f5
Mardanyan1 <70753243+Mardanyan1@users.noreply.github.com>
2020-09-25 12:45:24 +0300

    Create shape.java

---
+++ shape.java
@@ -0,0 +1,14 @@
+package ru.mirea.ikbo20.pr3;
+public abstract class Shape {
+    protected String color = "transparent";
+    protected boolean filled;
+    abstract public void Shape();
+    abstract public void Shape(String color, boolean filled);
+    abstract public String getColor();
+    abstract public void setColor(String color);
+    abstract public boolean isFilled();
+    abstract public void setFilled(boolean filled);
+    abstract public double getArea();
+    abstract public double getPerimeter();
+    abstract public String toString();
+}
